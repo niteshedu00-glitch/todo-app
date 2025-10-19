074e640241ddcaa860d684aa1eed0c34828a459b
Nitesh verma <nitesh.edu00@gmail.com>
2025-10-20 02:52:30 +0530

    Added Todo app with TaskWork and progress feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,48 @@ import TaskWork from "./components/TaskWork";
 const App = () => {
   const [todos, setTodos] = useState([]);
 
-  const updateProgress = (updatedTask, value) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.task === updatedTask.task ? { ...todo, progress: value } : todo
-      )
-    );
-  };
-
+  // Add a new todo
   const addTodo = (task) => {
     setTodos([...todos, { task, progress: 0, due: "2025-10-25" }]);
   };
 
+  // Delete a todo by index
   const deleteTodo = (index) => {
     setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  // Update progress of a todo
+  const updateProgress = (updatedTodo, value) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.task === updatedTodo.task ? { ...todo, progress: value } : todo
+      )
+    );
   };
 
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<TodoList todos={todos} deleteTodo={deleteTodo} />} />
+        {/* Home page: Todo List */}
+        <Route
+          path="/"
+          element={<TodoList todos={todos} deleteTodo={deleteTodo} />}
+        />
+
+        {/* Input page: Add new todo */}
         <Route path="/input" element={<TodoInput addTodo={addTodo} />} />
+
+        {/* Task page: Update progress */}
         <Route
           path="/task"
-          element={<TaskWork todo={todos[0]} updateProgress={updateProgress} />}
+          element={
+            todos[0] ? (
+              <TaskWork todo={todos[0]} updateProgress={updateProgress} />
+            ) : (
+              <TodoList todos={todos} deleteTodo={deleteTodo} />
+            )
+          }
         />
       </Routes>
     </Router>
